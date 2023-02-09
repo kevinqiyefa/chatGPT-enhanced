c1db7edce5c58e9d92689a345c92ca36d8a15dda
Kevin <kevinqiyf@hotmail.com>
2023-02-08 23:48:23 -0800

    display messages

--- components/Message.tsx
+++ components/Message.tsx
@@ -6,8 +6,9 @@ type Props = {
 };
 
 const Message = ({ message }: Props) => {
+  const isChatGPT = message.user.name === 'ChatGPT';
   return (
-    <div className="py-5 text-white">
+    <div className={`py-5 text-white ${isChatGPT && ' bg-[#434654]'}`}>
       <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
         <img src={message.user.avatar} alt="profile pic" className="h-8 w-8" />
         <p className="pt-1 text-sm">{message.text}</p>
